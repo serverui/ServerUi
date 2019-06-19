6b391163a4386d715d4112e7a9f785aeda48b637
astronomizestudios <51801389+astronomizestudios@users.noreply.github.com>
2019-06-18 19:26:09 -0600

    Update ServerUi.java

--- src/me/astronomize/serverui/ServerUi.java
+++ src/me/astronomize/serverui/ServerUi.java
@@ -4,7 +4,8 @@ import java.io.IOException;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import me.astronomize.serverui.gui.Init;;
+import me.astronomize.serverui.gui.ServerGui;
+import me.astronomize.serverui.updater.SpigotUpdater;;
 
 /*
  *   _________                                ____ ___.__ 
@@ -26,19 +27,31 @@ public class ServerUi extends JavaPlugin {
 	
 	public void onEnable() {
 		
+		
+	    SpigotUpdater updater = new SpigotUpdater(this, 68429);
+	    try {
+	        if (updater.checkForUpdates())
+	            getLogger().info("An update was found! New version: " + updater.getLatestVersion() + " download: " + updater.getResourceURL());
+	    } catch (Exception e) {
+	        getLogger().severe("Could not check for updates! Stacktrace:");
+	        e.printStackTrace();
+	    }
+		
 		// opens and creates the GUI
 		try {
-			Init.initGui();
+			ServerGui.initGui();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
 	}
 	
 	public void onDisable() {
 		
 		// closes the GUI
-		Init.closeGui();
+		ServerGui.closeGui();
 	}
 	
 }
