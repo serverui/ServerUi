ef222765b3850781b5344e23cbf6ff769ba70496
astronomizestudios <51801389+astronomizestudios@users.noreply.github.com>
2019-06-21 16:00:42 -0600

    Added a couple commands.
    Added /disable, /enable, /pl, and /serverui.

--- src/me/astronomize/serverui/ServerUi.java
+++ src/me/astronomize/serverui/ServerUi.java
@@ -4,8 +4,16 @@ import java.io.IOException;
 
 import javax.swing.UnsupportedLookAndFeelException;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import me.astronomize.serverui.exceptions.IncompatiblePluginException;
 import me.astronomize.serverui.gui.ServerGui;
 import me.astronomize.serverui.updater.SpigotUpdater;;
 
@@ -24,12 +32,39 @@ import me.astronomize.serverui.updater.SpigotUpdater;;
  *  functions:
  *  public void onEnable()
  *  public void onDisable()
+ *  public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+ *  
+ *  variables:
+ *  private String[] incompatiblePluginNames
+ *  private PluginDescriptionFile desc
  */
-public class ServerUi extends JavaPlugin {
+public class ServerUi extends JavaPlugin implements Listener {
+	
+	// the names of incompatible plugins.
+	private String[] incompatiblePluginNames = new String[] {"ProtocolLib"};
+	
+	private PluginDescriptionFile desc = this.getDescription();
 	
 	public void onEnable() {
 		
-	    SpigotUpdater updater = new SpigotUpdater(this, 68429);
+		getServer().getPluginManager().registerEvents(this, this);
+		
+		if(this.getConfig().getBoolean("auto-disable-incompatible-plugins")) {
+			for(Plugin p : Bukkit.getPluginManager().getPlugins()) {
+				for(String name : incompatiblePluginNames) {
+					if(p.getName().equalsIgnoreCase(name)) {
+						Bukkit.getPluginManager().disablePlugin(Bukkit.getPluginManager().getPlugin(name));
+						try {
+							
+							throw new IncompatiblePluginException("[ServerUi] The plugin; " + p.getName() + " is incompatible with ServerUi."
+									+ "\n Using it can possibly cause errors and other plugins to stop working.");
+						} catch (IncompatiblePluginException e) {}
+					}
+				}
+			}
+		}
+		
+		SpigotUpdater updater = new SpigotUpdater(this, 68429);
 	    try {
 	        if (updater.checkForUpdates()) {
 	            getLogger().info("An update was found! New version: " + updater.getLatestVersion() + "! download it here: " + updater.getResourceURL());
@@ -61,14 +96,71 @@ public class ServerUi extends JavaPlugin {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 
 	}
 	
 	public void onDisable() {
-		
-		// closes the GUI
 		ServerGui.closeGui();
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(cmd.getName().equalsIgnoreCase("serverui")) {
+			sender.sendMessage(ChatColor.GOLD + "----------------------------------------");
+			sender.sendMessage(ChatColor.GREEN + "ServerUi v" + ChatColor.DARK_GREEN + desc.getVersion() + ChatColor.GREEN + " by astronomize");
+			sender.sendMessage(ChatColor.GREEN + "Download link: " + ChatColor.DARK_GREEN + "https://www.spigotmc.org/resources/serverui.68429/");
+			try {
+				if(new SpigotUpdater(this, 68429).checkForUpdates()) {
+					sender.sendMessage(ChatColor.GREEN + "New version availiable: " + ChatColor.DARK_GREEN + " Yes");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			sender.sendMessage(ChatColor.GOLD + "----------------------------------------");
+			return true;
+		}
+		
+		if(cmd.getName().equalsIgnoreCase("pl")) {
+			String pluginList = "Plugins: ";
+			for(Plugin p : Bukkit.getPluginManager().getPlugins()) {
+				if(pluginList.equals("Plugins: ")) {
+					if(p.isEnabled()) {
+						pluginList = pluginList + ChatColor.DARK_GREEN + p.getName() + ":" + ChatColor.GREEN + " Enabled";
+					} else {
+						pluginList = pluginList + ChatColor.DARK_GREEN + p.getName() + ":" + ChatColor.RED + " Disabled";
+					}
+				} else {
+					if(p.isEnabled()) {
+						pluginList = pluginList + ", " + ChatColor.DARK_GREEN + p.getName() + ":" + ChatColor.GREEN + " Enabled";
+					} else {
+						pluginList = pluginList + ", " + ChatColor.DARK_GREEN + p.getName()  + ":" + ChatColor.RED + " Disabled";
+					}
+				}
+			}
+			sender.sendMessage(pluginList);
+		}
+		
+		if(cmd.getName().equalsIgnoreCase("enable")) {
+			if(args.length != 0) {
+				if(Bukkit.getPluginManager().getPlugin(args[0]) != null) {
+					Bukkit.getPluginManager().enablePlugin(Bukkit.getPluginManager().getPlugin(args[0]));
+					sender.sendMessage(ChatColor.GREEN + "Enabled the plugin: " + args[0]);
+				} else {
+					sender.sendMessage(ChatColor.RED + "This plugin doesn't exist!");
+				}
+			}
+		}
+		
+		if(cmd.getName().equalsIgnoreCase("disable")) {
+			if(args.length != 0) {
+				if(Bukkit.getPluginManager().getPlugin(args[0]) != null) {
+					Bukkit.getPluginManager().disablePlugin(Bukkit.getPluginManager().getPlugin(args[0]));
+					sender.sendMessage(ChatColor.GREEN + "Disabled the plugin: " + args[0]);
+				} else {
+					sender.sendMessage(ChatColor.RED + "This plugin doesn't exist!");
+				}
+			}
+		}
+		return false;
 	}
 	
 }
